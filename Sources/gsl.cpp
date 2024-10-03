ef48407c3a80e44b5f6e061ebba9deed4edd8ede
Charles K. Neimog <charlesneimog@outlook.com>
2024-10-03 12:36:52 -0300

    reorganize

--- Sources/gsl.cpp
+++
@@ -1,9 +0,0 @@
-#include <gsl/gsl_sf_bessel.h>
-#include <iostream>
-
-int main() {
-  double x = 5.0;
-  double y = gsl_sf_bessel_J0(x);
-  std::cout << "J0(" << x << ") = " << y << std::endl;
-  return 0;
-}
